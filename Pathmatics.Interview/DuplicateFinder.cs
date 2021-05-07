f83076eb419941bdec8dfb33ae1a31b96efabbae
Ryan Peterson <ryanpetersondesigns@gmail.com>
2021-05-06 21:07:11 -0700

    Add project files.

---
+++ Pathmatics.Interview/DuplicateFinder.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Pathmatics.Interview
+{
+    class DuplicateFinder
+    {
+        public List<string> Items { get; set; }
+
+        public Dictionary<string, List<string>> GetDuplicates()
+        {
+            List<string> workingList = new(Items);
+            Dictionary<string, List<string>> duplicates = new();
+
+            //You'll find that the naming doesn't get much better than "item" and "items"
+            //I'm sure we can come up with something better together
+            foreach (string item in Items)
+            {
+                //Get All exact alphabetical duplicates first
+                //Please forgive the use of "x" creativity in naming was lacking against the clock
+                if (workingList.Where(x => Sanitize(x) == Sanitize(item)).Count() > 1)
+                    AddThingsLike(workingList, duplicates, item);
+                //Then get anything like that item
+                else if (workingList.Any(x => IsLike(x, item)))
+                    AddThingsLike(workingList, duplicates, item);
+            }
+            
+            return duplicates;
+        }
+
+        private void AddThingsLike(List<string> workingList, Dictionary<string, List<string>> duplicates, string item)
+        {
+            duplicates.Add(item, new List<string>());
+            //"Thing" is admittedly generic, I'm sure there's an obviously better name
+            foreach (string thing in ThingsLike(workingList, item))
+            {
+                Console.WriteLine(string.Concat(item, ": ", thing));
+                //adding independantly so we can...
+                duplicates[item].Add(thing);
+                //remove from the list and hopefully make things a bit faster and avoid duplication
+                workingList.Remove(thing);
+            }
+        }
+
+        private static List<string> ThingsLike(List<string> workingList, string item)
+        {
+            return workingList.Where(x=> IsLike(x, item)).ToList();
+        }
+        
+        //Just take alphabetical characters
+        private static readonly Regex AlphaFilters = new(@"[^A-Z]", RegexOptions.Compiled);
+        private static string Sanitize(string x) => AlphaFilters.Replace((x ?? string.Empty).ToUpper(), string.Empty);
+
+        private static bool IsLike(string x, string item)
+        {
+            //Get each distinct word
+            string[] xArray = x.Split(" ");
+            string scrubbedItem = Sanitize(item);
+            //Avoid A, An, The, Of, in, etc.
+            //In a perfect world I'd find a dictionary of common conjunctions, articles, etc. to avoid
+            foreach (string xString in xArray.Where(x=>x.Length>3))
+                //Definitely too broad, need to make criteria more specific, weighted scores etc.
+                if (scrubbedItem.Contains(xString.ToUpper()))
+                    return true;
+                else
+                {
+                    //Get each distinct word
+                    string[] itemArray = item.Split(" ");
+                    string scrubbedX = Sanitize(x);
+                    //Avoid A, An, The, Of, in, etc.
+                    //In a perfect world I'd find a dictionary of common conjunctions, articles, etc. to avoid
+                    foreach(string itemString in itemArray.Where(x=>x.Length>3))
+                        //Definitely too broad, need to make criteria more specific, weighted scores etc.
+                        if (scrubbedX.Contains(itemString.ToUpper()))
+                            return true;
+                }
+
+            return false;
+        }
+    }
+}
---
+++ Pathmatics.Interview/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace Pathmatics.Interview
+{
+    class Program
+    {
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            HttpClient client = new();
+            //forgive the fact that it doesn't /technically/ take a text file.
+            //I figure having a url may even be better depending on the use case...
+            //Like this one! The file is stored on an accessible s3 blob, maybe it's updated periodically, who knows...
+            //Code borrowed from https://docs.microsoft.com/en-us/dotnet/api/system.net.http.httpclient?view=net-5.0
+            HttpResponseMessage response = await client.GetAsync("https://s3.amazonaws.com/ym-hosting/tomtest/advertisers.txt");
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+            DuplicateFinder duplicateFinder = new();
+            duplicateFinder.Items = new (responseBody.Split(Environment.NewLine));
+            foreach (KeyValuePair<string, List<string>> item in duplicateFinder.GetDuplicates())
+                Console.WriteLine(string.Concat(item.Key, ": ", string.Join(',', item.Value)));
+        }
+    }
+}
